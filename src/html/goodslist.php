6dfe8af032d67ae51224dff3404a60f04c706869
ActiveKid <1510697974@qq.com>
2017-02-06 00:12:35 +0800

    分页请求成功实现

--- src/html/goodslist.php
+++ src/html/goodslist.php
@@ -1,20 +1,7 @@
 <?php
 	include 'DBHelper.php';
-     $page = (int)$_POST["page"];
-     $pageSize = 20;
-     $rowCount = 0;
-     $pageNum = ($page-1)* $pageSize;
-     $pageCount = 0;
-     $sql1 = "select count(id) from tb4";
-	 $res1 = mysql_query($sql1);
-	   
-	 if($row1=mysql_fetch_row($res1)){
-	   $rowCount = $row1[0];
-	 }
-	   
-	 //计算共有多少页，ceil取进1
-	 $pageCount = ceil(($rowCount/$pageSize));
-
-     $sql = ("SELECT * FROM  tb4 limit $pageNum, $pageSize");
+    $page = (int)$_POST["page"];
+     $pageNum = ($page-1)*20;
+     $sql = ("SELECT * FROM  tb4 limit $pageNum,20");
      query($sql);
 ?>
--- src/js/productlist.js
+++ src/js/productlist.js
@@ -22,6 +22,7 @@
 	xhr.send();*/
 	;(function($){
 		$(function(){
+			//默认第一页
 			$.post('../html/goodslist.php',{page:1},function(response){
 				console.log(response);
 				var res = JSON.parse(response);
@@ -31,7 +32,32 @@
 					var $li = $('<li/>').addClass('inline').html('<div class="pic"><img src="'+item.src+'"/></div><p>' + item.price + '</p><p>成交：' + item.counts + '笔</p><p>' + item.name + '</p>');
 					$ul.append($li);
 				});
-				$('body').append($ul);
-			})
+				$('.goodlist').append($ul);
+				//$('<div/>').addClass('page').html('<span class="clk">1</span><span>2</span>').appendTo('body');
+			});
+			//点击分页
+			$('.page span').on('click',function(){
+				
+				$('.goodlist').empty();
+				var $self = $(this);
+				//点击span高亮
+				$self.parent('.page').children().removeClass('clk');
+				$self.addClass('clk');
+				//点击分页
+				//请求数据
+				$.post('../html/goodslist.php',{page:$self.html()},function(response){
+					console.log(response);
+					var res = JSON.parse(response);
+					var $ul = $('<ul/>').addClass('wrap');
+					$.each(res,function(idx,item){
+						
+						var $li = $('<li/>').addClass('inline').html('<div class="pic"><img src="'+item.src+'"/></div><p>' + item.price + '</p><p>成交：' + item.counts + '笔</p><p>' + item.name + '</p>');
+						$ul.append($li);
+					});
+					$('.goodlist').append($ul);
+				});
+			});
+			
 		});
+		
 	})(jQuery);
