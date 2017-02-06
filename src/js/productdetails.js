0f04c2d2c46259d1e799141bf92855386346b87f
ActiveKid <1510697974@qq.com>
2017-02-06 21:02:35 +0800

    add car and productdetails html

--- src/js/productdetails.js
+++ src/js/productdetails.js
@@ -2,9 +2,13 @@ window.onload = function(){
 		var con_fangDaIMg = document.getElementsByClassName('con-fangDaIMg')[0];
 		var de_center = document.getElementsByClassName('de_center')[0];
 		var jd_price = document.getElementById('jd-price');
+		var count = document.getElementById('count');
+		var jian = document.getElementById('jian');
+		var jia = document.getElementById('jia');
 		// 用于保存购物车商品信息
 		var carList = [];
-
+		var countH = parseInt(count.innerHTML);
+		console.log(countH);
 		// 先获取当前cookie，如果没设置cookie有效时间，第一次进来为空，关闭浏览器也清零
 		var cookies = document.cookie.split('; ');
 		for(var i=0;i<cookies.length;i++){
@@ -12,6 +16,20 @@ window.onload = function(){
 			if(arr[0] === 'carlist'){
 				carList = JSON.parse(arr[1]);
 			}
+		}
+		//数量加减
+		jian.onclick = function(){
+			countH--;
+			console.log('jian');
+			if(countH <= 0){
+				countH === 0;
+			}
+			count.innerHTML = countH;
+		}
+		jia.onclick = function(){
+			console.log('jia');
+			countH++;
+			count.innerHTML = countH;
 		}
 
 		// 事件委托
@@ -23,7 +41,7 @@ window.onload = function(){
 			if(target.tagName.toLowerCase() === 'button'){
 				// 先创建一个对象保存当前被点击的商品信息
 				var goodsObj = {};
-				goodsObj.qty = 1;
+				goodsObj.qty = countH;
 				goodsObj.name = de_center.children[0].innerHTML;
 				goodsObj.price = jd_price.innerHTML;
 				goodsObj.imgUrl = con_fangDaIMg.children[0].src;
@@ -32,15 +50,16 @@ window.onload = function(){
 				if(carList.length===0){
 					// 添加到carList
 					carList.push(goodsObj);
-				}else{
+				}/*else{
 					
 					carList[0].qty++;
+					//countH++;
 						
-					}	
+					}*/	
 				}
 				
 				
-				console.log(carList);
+				//console.log(carList);
 				// 存入cookie
 				// 把对象/数组转换诚json字符串：JSON.stringify()
 				document.cookie = 'carlist=' + JSON.stringify(carList);
